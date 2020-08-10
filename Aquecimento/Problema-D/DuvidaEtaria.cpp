7bfdd6551c93e0571b16d1b0ffd8e6cf2bd2484b
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-08-10 07:16:25 -0300

    Adicionada solucao do D e corrijida as descrições

---
+++ Aquecimento/Problema-D/DuvidaEtaria.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string nome, hoje, nasc;
+    int ano_hoje, ano_nasc, idade;
+
+    cin>>nome;
+    cin>>hoje;
+    cin>>nasc;
+
+    ano_hoje = atoi(hoje.substr(6, 4).c_str());
+    ano_nasc = atoi(nasc.substr(6, 4).c_str());
+    
+    idade = ano_hoje - ano_nasc;
+
+    hoje = hoje.substr(3,2) + hoje.substr(0, 2);
+    nasc = nasc.substr(3,2) + nasc.substr(0, 2);
+
+    if(hoje < nasc)
+        idade--;
+    if(hoje == nasc)
+        cout<<"Feliz aniversario!"<<endl;
+    cout<<"Voce tem "<<idade<<" anos "<<nome<<"."<<endl;
+    
+    return 0;
+}
